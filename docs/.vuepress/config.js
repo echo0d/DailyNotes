5ed1ce87fb852bf0de7ca87baf52e92d786052ac
echo0d <echo0d@163.com>
2023-12-22 14:20:10 +0800

    init

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,7 +2,7 @@ module.exports = {
     // 站点的标题
     title: "echo0d-DailyNotes",
     // 站点的描述
-    description: "为将来留一种记录",
+    description: "DailyNotes",
     // 站点配置, 设置为 /[仓库名]/
     base: '/DailyNotes/',
 }
