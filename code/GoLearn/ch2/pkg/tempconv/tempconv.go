49efe66b872a1ef9c673d5546163a8c5ab1f5c60
echo0d <echo0d@163.com>
2024-03-23 18:12:01 +0800

    3.23备份go-ch2

---
+++ code/GoLearn/ch2/pkg/tempconv/tempconv.go
@@ -0,0 +1,18 @@
+package tempconv
+
+import "fmt"
+
+type Celsius float64    // 摄氏温度
+type Fahrenheit float64 // 华氏温度
+
+const (
+	AbsoluteZeroC Celsius = -273.15 // 绝对零度
+	FreezingC     Celsius = 0       // 结冰点温度
+	BoilingC      Celsius = 100     // 沸水温度
+)
+
+func CToF(c Celsius) Fahrenheit { return Fahrenheit(c*9/5 + 32) }
+
+func FToC(f Fahrenheit) Celsius { return Celsius((f - 32) * 5 / 9) }
+
+func (c Celsius) String() string { return fmt.Sprintf("%g°C", c) }
