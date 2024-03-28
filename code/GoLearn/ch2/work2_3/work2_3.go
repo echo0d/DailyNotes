2a6a0b5bf6dbf7fc63ecdf79a74bcdbb8e5c88eb
echo0d <echo0d@163.com>
2024-03-28 16:21:33 +0800

    ch2

--- code/GoLearn/ch2/work2_3/work2_3.go
+++ code/GoLearn/ch2/work2_3/work2_3.go
@@ -6,7 +6,10 @@ import (
 )
 
 func main() {
-	x := uint64(189264826)
+	x := uint64(18446744073709551614)
 	fmt.Printf("count: %d\n", popcount.PopCount(x))
 	fmt.Printf("count: %d\n", popcount.LPopCount(x))
+	fmt.Printf("count: %d\n", popcount.SPopCount(x))
+	fmt.Printf("count: %d\n", popcount.CPopCount(x))
+
 }
